a2c7beca5d583ef7fa970f6f04da1812eb3de760
usagigensui <usagigensuii@gmail.com>
2021-07-31 09:11:07 +0000

    メール機能修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # メール送信者情報
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
