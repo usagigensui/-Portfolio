51d45a4af6cc690f22e63d28783bbe3ccdc04bfc
usagigensui <usagigensuii@gmail.com>
2021-08-15 15:14:50 +0000

    投稿テキストのハイパーリンク対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,36 @@
 module ApplicationHelper
+  # URIライブラリの呼び出し
+  require 'uri'
+
+  # 投稿テキストのリンクやメルアドをリンク化
+  def user_text(text)
+    return if text.blank?
+
+    email2a(url2a(nl2br(sanitize(text).to_s)))
+  end
+
+  # 投稿テキストの改行を反映
+  def nl2br(text)
+    text.gsub(/\R/) { tag.br }
+    # text.gsub(/\R/) { %(<br>) }
+  end
+
+  # 投稿テキストのURLをハイパーリンクに変更
+  def url2a(text)
+    text.gsub(URI::DEFAULT_PARSER.make_regexp(%w[http https])) do
+      tag.a Regexp.last_match(0), href: Regexp.last_match(0), target: '_blank'
+      # %(<a href="#{Regexp.last_match(0)}" target="_blank">#{Regexp.last_match(0)}</a>)
+    end
+  end
+
+  # 投稿テキストのメールアドレスをリンクに変更
+  def email2a(text)
+    text.gsub(/[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i) do
+      tag.a Regexp.last_match(0), href: "mailto:#{Regexp.last_match(0)}"
+      # %(<a href="mailto:#{Regexp.last_match(0)}">#{Regexp.last_match(0)}</a>)
+    end
+  end
+
   # プロフィールオーナーのログイン確認
   def current_user_signed_in?(profile)
     user_signed_in? && current_user.id == profile.user_id
