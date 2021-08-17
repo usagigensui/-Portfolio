bbd80e76b620f5e4038b96fa12465ff851d76518
usagigensui <usagigensuii@gmail.com>
2021-08-17 15:19:06 +0000

    カラーを追加

--- app/models/color.rb
+++ app/models/color.rb
@@ -10,7 +10,7 @@ class Color < ApplicationRecord
   end
 
   # enum管理
-  enum text: { black: 0, white: 1 }, _prefix: true
-  enum background: { white: 0, black: 1 }, _prefix: true
-  enum accent: { blue: 0, red: 1 }, _prefix: true
+  enum text: { black: 0, white: 1 , sepia: 2}, _prefix: true
+  enum background: { white: 0, black: 1, gray: 2, ivory: 3, brown: 4 }, _prefix: true
+  enum accent: { lightgray: 0, darkgray: 1, blue: 2, red: 3, pink: 4, skyblue: 5, lightgreen: 6, yellow: 7, orange: 8 }, _prefix: true
 end
